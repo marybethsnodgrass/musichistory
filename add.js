f737682848b7f8a25c3b45cab104eaf8324a097d
msnodgrass25 <msnodgrass25@gmail.com>
2015-10-21 14:18:21 -0500

    updated some js

---
+++ add.js
@@ -0,0 +1,19 @@
+/*
+// Create event handler for HTML DOM element
+1. var <element name> = document.getElementById("<element ID>");
+2. <element name>.<event> = <function name>;
+3. function <function name> {      };
+*/
+
+
+var addMusicLink = document.getElementById("add-music-link");
+var addMusicView = document.getElementById("add-music");
+
+addMusicLink.addEventListener("click", function() {
+	addMusicView.classList.add("visible");
+	addMusicView.classList.remove("hidden");
+
+	listView.classList.add("hidden");
+
+})
+
--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-var songs = [];
+// var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songs[songs.length] = "Slide > by Goo Goo Dolls on the album Dizzy Up the Girl"
-songs.unshift = "Kerosene > by Miranda Lambert on the album Kerosene"
+// songs[songs.length] = "Slide > by Goo Goo Dolls on the album Dizzy Up the Girl"
+// songs.unshift = "Kerosene > by Miranda Lambert on the album Kerosene"
 
-var song0element = document.getElementById('song0');
-var song0text = song0element.innerHTML;
+// var song0element = document.getElementById('song0');
+// var song0text = song0element.innerHTML;
 
-song0text = songs[0];
-song0element.innerHTML = song0text;
+// song0text = songs[0];
+// song0element.innerHTML = song0text;
 
+
+var listLink = document.getElementById("list-music-link");
+var listView = document.getElementById("list-music");
+
+listLink.addEventListener("click", function() {
+	listView.classList.add("visible");
+	listView.classList.remove("hidden");
+
+	addMusicView.classList.add("hidden");
+})
